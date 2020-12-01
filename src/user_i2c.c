2e5e8faaaf584e3401454c26cc60a312f0b85b3f
vamoosebbf <vamoosebbf@qq,com>
2020-12-01 14:10:03 +0800

    fix memory leak

--- src/user_i2c.c
+++ src/user_i2c.c
@@ -28,29 +28,36 @@ int8_t bme280_i2c_read(uint8_t reg_addr, uint8_t *data, uint32_t len, void *intf
 int8_t bme280_i2c_write(uint8_t reg_addr, const uint8_t *data, uint32_t len, void *intf_ptr)
 {
     uint8_t addr;
-    uint8_t *w_buf = (uint8_t *)malloc(len+1);
+    uint8_t *w_buf = (uint8_t *)malloc(len + 1);
+    int ret;
     addr = *((uint8_t *)intf_ptr);
     w_buf[0] = reg_addr;
     memcpy(w_buf[1], data, len);
-    return maix_i2c_send_data(WEATHER_I2C_DEVICE, addr, w_buf, len+1, 5);
+    ret = maix_i2c_send_data(WEATHER_I2C_DEVICE, addr, w_buf, len + 1, 5);
+    free(w_buf);
+    return ret;
 }
 
-void qmc_writeto_mem(uint8_t reg, uint8_t data){
+void qmc_writeto_mem(uint8_t reg, uint8_t data)
+{
     int8_t ret = 0;
     uint8_t buf[2] = {reg, data};
     ret = maix_i2c_send_data(WEATHER_I2C_DEVICE, QMCX983_I2CADDR, buf, 2, 5);
-    if(ret != 0){
-        printf("send reg: %x %x failed\r\n",reg, data);
+    if (ret != 0)
+    {
+        printf("send reg: %x %x failed\r\n", reg, data);
     }
     return;
 }
 
-uint8_t qmc_readfrom_mem(uint8_t reg){
+uint8_t qmc_readfrom_mem(uint8_t reg)
+{
     uint8_t res;
     int8_t ret = 0;
     ret = maix_i2c_recv_data(WEATHER_I2C_DEVICE, QMCX983_I2CADDR, &reg, 1, &res, 1, 5);
-    if(ret != 0){
-        printf("recv reg: %x failed\r\n",reg);
+    if (ret != 0)
+    {
+        printf("recv reg: %x failed\r\n", reg);
     }
     return res;
 }
